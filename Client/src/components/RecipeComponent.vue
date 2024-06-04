6b6fb32840b1c4b507f02432f855a6d78524204b
Kertis <kertismwanza@gmail.com>
2024-06-04 09:28:34 +0200

    Properties angelegt, Groessen angepasst

--- Client/src/components/RecipeComponent.vue
+++ Client/src/components/RecipeComponent.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="previewBar">
         <div class="previewBackground">
-            <h1>Burger des Jahrtausends</h1>
+            <h1>{{ recipe_title }}</h1>
             <div class="recipeInformationContainer">
                 <img src="../assets/user_icon.png" class="userIconRecipe recipeShortInformation recipeIconInfos">
-                <p class="textInformationRecipe recipeShortInformation">manfred_kocht33</p>
+                <p class="textInformationRecipe recipeShortInformation">{{ recipe_author }}</p>
                 <img src="../assets/price_icon.png" class="priceIconRecipe recipeShortInformation recipeIconInfos">
-                <p class="textInformationRecipe recipeShortInformation">5$</p>
+                <p class="textInformationRecipe recipeShortInformation">{{ recipe_price }}€</p>
                 <img src="../assets/time_icon.png" class="timeIconRecipe recipeShortInformation recipeIconInfos">
-                <p class="textInformationRecipe recipeShortInformation">10min</p>
+                <p class="textInformationRecipe recipeShortInformation">{{ recipe_time }}min</p>
             </div>
             <div class="tagBoxesContainer">
                 <div>
@@ -42,22 +42,11 @@
             <img src="../assets/Thumbs Down.png"></img>
         </div>
     </div>
-    <div class="RecipeDetails">
-        <div class="ingredientContainer">
-            <h2 class="ingredients">Zutaten</h2>
-                <div class="ingredientList">
-                    <p>400g Hackfleisch <br>2TL Salz<br>2TL Pfeffer<br>Bratfett</p>
-                </div>
-                <h2 class="preperation">Zubereitung</h2>
-                <p>1. Wasser mit Salz aufsetzen, derweil Kartoffeln schälen und in kleine Stücke schneiden. 
-Die Kartoffeln nun ins Wasser geben und weich kochen. Die Butter hinzu geben und mit einem         Stampfer vermengen und zerstampfen, mit Salz und Pfeffer abschmecken.</p>
-                    <p>2. Eine Pfanne mit Butter erhitzen und gefrorene Fischstäbchen hinzugeben. 
-So lange braten bis die Fischstäbchen goldbraun angebraten sind. Optional mit Salz und Zitrone verfeinern.</p>
-                        <p>3. Den gefrorenen Rahmspinat im Topf auf mittlerer Hitze erwärmen und einige Minuten köcheln lassen. Mit Pfeffer abschmecken.</p>
-        </div>
-
-    </div>
 </template>
+
+<script setup>
+    defineProps(['recipe_title', 'recipe_image', 'recipe_author', 'recipe_price', 'recipe_time'])
+</script>
 
 <style>
 .previewBar{
@@ -148,28 +137,4 @@ So lange braten bis die Fischstäbchen goldbraun angebraten sind. Optional mit S
     background-color: white;
     margin: 10px;
 }
-.ingredientContainer h2{
-    text-align: center;
-    font-weight: bold;
-    font-size: 200%;
-    padding-top: 20px
-}
-.ingredientContainer p{
-    text-align: center;
-    font-size: 140%
-}
-
-.recipeShortInformation{
-    height: 40%;
-    margin: auto;
-}
-.textInformationRecipe{
-    size: 20%;
-    padding-right: 4%;
-}
-.recipeIconInfos{
-    padding-left: 4%
-}
-
-
 </style>
--- Client/src/views/RecipeView.vue
+++ Client/src/views/RecipeView.vue
@@ -5,7 +5,42 @@ import RecipeComponent from '@/components/RecipeComponent.vue';
 
 <template>
     <RecipeComponent></RecipeComponent>
-    <div>
-        
+    <div class="RecipeDetails">
+        <div class="ingredientContainer">
+            <h2 class="ingredients">Zutaten</h2>
+                <div class="ingredientList">
+                    <p>400g Hackfleisch <br>2TL Salz<br>2TL Pfeffer<br>Bratfett</p>
+                </div>
+                <h2 class="preperation">Zubereitung</h2>
+                <p>1. Wasser mit Salz aufsetzen, derweil Kartoffeln schälen und in kleine Stücke schneiden. 
+Die Kartoffeln nun ins Wasser geben und weich kochen. Die Butter hinzu geben und mit einem         Stampfer vermengen und zerstampfen, mit Salz und Pfeffer abschmecken.</p>
+                    <p>2. Eine Pfanne mit Butter erhitzen und gefrorene Fischstäbchen hinzugeben. 
+So lange braten bis die Fischstäbchen goldbraun angebraten sind. Optional mit Salz und Zitrone verfeinern.</p>
+                        <p>3. Den gefrorenen Rahmspinat im Topf auf mittlerer Hitze erwärmen und einige Minuten köcheln lassen. Mit Pfeffer abschmecken.</p>
+        </div>
     </div>
 </template>
+<style>
+.ingredientContainer h2{
+    text-align: center;
+    font-weight: bold;
+    font-size: 200%;
+    padding-top: 20px
+}
+.ingredientContainer p{
+    text-align: center;
+    font-size: 140%
+}
+
+.recipeShortInformation{
+    height: 40%;
+    margin: auto;
+}
+.textInformationRecipe{
+    size: 20%;
+    padding-right: 4%;
+}
+.recipeIconInfos{
+    padding-left: 4%
+}
+</style>
