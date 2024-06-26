737a9ea084d7046e3b8268e69b7b85aad7ab6507
PASULGE <paul.sge.eintracht@gmail.com>
2024-06-26 23:05:09 +0200

    payload login und auth überarbeitet. Den Auth angepasst

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -4,22 +4,22 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const KEY = process.env.HASHKEY;
 const TOKEN_EXPIRATION_TIME = process.env.EXPIRATION;
+const jwtHeader = { algorithm: "HS256" };
 
 const registierung = async (req, res) => {
   try {
     const { username, password } = req.body;
     //console.log( await Profile.findOne({name: username}));
-    if(await Profile.findOne({name: username})){
-      res.status(500).json({message : "Nutzer schon vorhanden"})
-    }else{
+    if (await Profile.findOne({ name: username })) {
+      res.status(500).json({ message: "Nutzer schon vorhanden" });
+    } else {
       const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = {
-      name: username,
-      password: hashedPassword,
-    };
-    const success = await Profile.create(newUser);
-    console.log(success._id.toString())
-    res.status(200).json(success);
+      const newUser = {
+        name: username,
+        password: hashedPassword,
+      };
+      const success = await Profile.create(newUser);
+      res.status(200).json(success);
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -29,16 +29,14 @@ const registierung = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
+    let expirationTime = Date.now() / 1000 + parseInt(TOKEN_EXPIRATION_TIME);
+    let payload = {
+      username: username,
+      exp: expirationTime,
+    };
     const user = await Profile.findOne({ name: username }, "name password");
-    if (
-      user.password &&
-      (await bcrypt.compare(password, user.password))
-    ) {
-      const token = jwt.sign({ username }, KEY, {
-        expiresIn: TOKEN_EXPIRATION_TIME,
-      });
-      let a = await Profile.findOne({name : JSON.parse(Buffer.from(token.split('.')[1],'base64').toString()).username})
-      console.log(a.name)
+    if (user.password && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(payload, KEY, jwtHeader);
       res.json({ token });
     } else {
       res.status(401).json({ message: "Invalid credentials" });
--- Server/util/authentication.js
+++ Server/util/authentication.js
@@ -1,28 +1,37 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/profile.model");
 require("dotenv").config();
 const KEY = process.env.HASHKEY;
 const TOKEN_EXPIRATION_TIME = process.env.EXPIRATION;
+const jwtHeader = { algorithm: "HS256" };
 
-const authenticateJWT = (req, res, next) => {
+const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+  let expirationTime = Date.now() / 1000 + parseInt(TOKEN_EXPIRATION_TIME);
 
   if (authHeader) {
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, KEY, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      const newToken = jwt.sign({ username: user.username }, KEY, {
-        expiresIn: TOKEN_EXPIRATION_TIME,
+    try{
+      let userCheck = await User.findOne({
+        name: jwt.verify(authHeader, KEY, jwtHeader).username,
       });
-      res.setHeader("Authorization", `Bearer ${newToken}`);
-      req.newToken = newToken;
-      req.user = user;
-      next();
-    });
+      if (userCheck !== null) {
+        let payload = {
+          username: jwt.verify(authHeader, KEY, jwtHeader).username,
+          exp: expirationTime,
+        };
+        const newToken = jwt.sign(payload, KEY, jwtHeader);
+        res.setHeader("Authorization", `Bearer ${newToken}`);
+        req.newToken = newToken;
+        req.user = jwt.verify(authHeader, KEY, jwtHeader).username;
+        next();
+      } else {
+        res.json({ message: "Nutzer nicht erkannt" });
+      }
+    }catch(error){
+      res.status(401).json({ message: "Kein gueltiger Token / Session abgelaufen" });
+    }
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401).json({ message: "Nicht eingeloggt" });
   }
 };
 
